c2ed050ba2505f1e7fa08cb341cb3db4e00a7ce2
lwcincr93 <lwcincr93@gmail.com>
2015-11-26 15:10:15 -0600

    Updated Envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,15 +1,25 @@
 @servers(['buzz' => 'lwcincr93@104.236.240.40'])
 
 @task('full-deploy', ['on' => 'buzz'])
-cd /var/www/master
+cd /var/www/monster
 git pull origin {{ isset($branch) ? $branch : 'master' }}
 composer install
 npm install
-gulp
+gulp --production
 @endtask
 
 @task('quick-deploy', ['on' => 'buzz'])
-cd /var/www/master
+cd /var/www/monster
 git pull origin {{ isset($branch) ? $branch : 'master' }}
-gulp
+gulp --production
+@endtask
+
+@task('migrate', ['on' => 'buzz'])
+cd /var/www/monster
+php artisan migrate
+@endtask
+
+@task('migrate-refresh', ['on' => 'buzz'])
+cd /var/www/monster
+php artisan migrate:refresh
 @endtask
