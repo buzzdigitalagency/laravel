09706f878eea3e200712ec43d79e718f2c3732d0
lwcincr93 <lwcincr93@gmail.com>
2015-11-26 15:09:10 -0600

    Added animate.css and SB Admin 2 theme to bower and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,32 @@ require('laravel-elixir-livereload');
 elixir(
     function (mix) {
         mix
+        //Basic
             .sass('app.scss')
             .sass('packages/pace.scss')
+            .styles([
+                '../bower/animate.css/animate.css'
+            ], 'public/css/animate.css')
+            .scripts([
+                '../bower/jquery/dist/jquery.js',
+                '../bower/bootstrap-sass/assets/javascripts/bootstrap.js'
+            ], 'public/js/app.js')
+            //Admin
+            .sass('admin.scss')
+            .styles([
+                '../../../public/css/admin.css',
+                '../bower/metisMenu/src/metisMenu.css',
+                '../bower/startbootstrap-sb-admin-2/dist/css/sb-admin-2.css'
+            ], 'public/css/admin.css')
             .scripts([
                 '../bower/jquery/dist/jquery.js',
                 '../bower/bootstrap-sass/assets/javascripts/bootstrap.js',
-            ], 'public/js/app.js')
+                '../bower/metisMenu/src/metisMenu.js',
+                '../bower/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js'
+            ], 'public/js/admin.js')
+            //Aditional
+            .version(['css/app.css', 'css/admin.css', 'js/admin.js', 'js/app.js'])
+            .copy('public/fonts', 'public/build/fonts')
             .imagemin(
                 'resources/assets/imgs', 'public/imgs',
                 {
